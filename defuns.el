e411fc64d68f653e1c547b1c0ea86cad2bc511d5
Mario Fernandez <mario.fernandez@friendscout24.de>
2010-12-22 15:49:42 +0100

    fixed indentation

--- defuns.el
+++ defuns.el
@@ -145,37 +145,37 @@ Symbols matching the text at point are put first in the completion list."
 ;; Credit to
 ;; http://groups.google.com/group/gnu.emacs.help/msg/a784fbb684a24e17
 (defun move-text-internal (arg)
-   (cond
-    ((and mark-active transient-mark-mode)
-     (if (> (point) (mark))
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
         (exchange-point-and-mark))
-     (let ((column (current-column))
+    (let ((column (current-column))
           (text (delete-and-extract-region (point) (mark))))
-       (forward-line arg)
-       (move-to-column column t)
-       (set-mark (point))
-       (insert text)
-       (exchange-point-and-mark)
-       (setq deactivate-mark nil)))
-    (t
-     (beginning-of-line)
-     (when (or (> arg 0) (not (bobp)))
-       (forward-line)
-       (when (or (< arg 0) (not (eobp)))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (beginning-of-line)
+    (when (or (> arg 0) (not (bobp)))
+      (forward-line)
+      (when (or (< arg 0) (not (eobp)))
         (transpose-lines arg))
-       (forward-line -1)))))
+      (forward-line -1)))))
 
 (defun move-text-down (arg)
-   "Move region (transient-mark-mode active) or current line
+  "Move region (transient-mark-mode active) or current line
   arg lines down."
-   (interactive "*p")
-   (move-text-internal arg))
+  (interactive "*p")
+  (move-text-internal arg))
 
 (defun move-text-up (arg)
-   "Move region (transient-mark-mode active) or current line
+  "Move region (transient-mark-mode active) or current line
   arg lines up."
-   (interactive "*p")
-   (move-text-internal (- arg))) 
+  (interactive "*p")
+  (move-text-internal (- arg)))
 
 ;;; CODING-HOOKS
 
