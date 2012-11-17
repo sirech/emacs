448a6feeec4aebd03a716b377893aaba4a2a7b28
Mario Fernandez <mario.fernandez@hceris.com>
2012-11-17 14:41:22 +0100

    FIX 2: don't set window size

--- machines/finks.el
+++ machines/finks.el
@@ -1,10 +1,5 @@
 ;;; finks.el --- Settings specific to a machine
 
-;; (set-frame-size-according-to-resolution)
-(setq initial-frame-alist
-      '((left . 0) (top . 0)
-	(width . 329) (height . 111)))
-
 ;; Export env variables
 (setenv "WORKSPACE" "~/Documents/workspace")
 (setenv "RSENSE_HOME" "/opt/local/share/rsense-0.3")
