6c40268670172ba7d9bfedb9d2dc2e5b40d204ce
Mario Fernandez <mario.fernandez@hceris.com>
2013-01-03 01:03:47 +0100

    Add some helpers for git

--- git-config.el
+++ git-config.el
@@ -3,7 +3,36 @@
 (eval-after-load 'magit
   '(progn
      (set-face-foreground 'magit-diff-add "green3")
-     (set-face-foreground 'magit-diff-del "red3")))
+     (set-face-foreground 'magit-diff-del "red3")
+     (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+     (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)))
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
 
 (provide 'git-config)
 ;;; git-config.el ends here
