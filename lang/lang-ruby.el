51ac91a201adb5760d2e9c13ed4972a47cd5dbb7
Mario Fernandez <mario.fernandez@hceris.com>
2013-02-14 23:14:26 +0100

    Remove old keybinding for ruby

--- lang/lang-ruby.el
+++ lang/lang-ruby.el
@@ -137,7 +137,6 @@
      ;; (add-hook 'ruby-mode-hook 'inf-ruby-keys)
      ;; (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
      ;; (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
-     (define-key ruby-mode-map (kbd "M-.") 'rsense-jump-to-definition)
      (define-key ruby-mode-map (kbd "C-c l") "lambda")))
 
 ;; (global-set-key (kbd "C-h r") 'ri)
