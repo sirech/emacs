8b323cd691deee065c9a8a7d2f16ba9ff463fc73
Mario Fernandez <mario.fernandez@friendscout24.de>
2013-04-22 15:51:25 +0200

    Add ask google func

--- defuns/defuns-misc.el
+++ defuns/defuns-misc.el
@@ -14,6 +14,16 @@
     (cond ((search-forward "<?xml" nil t) (xml-mode))
           ((search-forward "<html" nil t) (html-mode)))))
 
+(defun google ()
+  "Google the selected region if any, display a query prompt otherwise."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string (if mark-active
+         (buffer-substring (region-beginning) (region-end))
+       (read-string "Google: "))))))
+
 ;; Cosmetic
 
 (defun pretty-lambdas ()
