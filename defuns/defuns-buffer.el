0652d21bff9ad533880ac5bafc710df474f3627d
Mario Fernandez <mario.fernandez@friendscout24.de>
2013-04-22 16:02:16 +0200

    Add copy file name to clipboard

--- defuns/defuns-buffer.el
+++ defuns/defuns-buffer.el
@@ -183,4 +183,14 @@ Don't mess with special buffers."
       (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
 (provide 'defuns-buffer)
