b8b70ddda637452681599aa141c9229866adfbee
Mario Fernandez <mario.fernandez@amiando.com>
2013-06-06 18:03:11 +0200

    Indent with enter in ruby

--- lang/lang-ruby.el
+++ lang/lang-ruby.el
@@ -106,7 +106,7 @@
 
      ;; (setq ruby-use-encoding-map nil)
      ;; (add-hook 'ruby-mode-hook 'inf-ruby-keys)
-     ;; (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
      ;; (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
      (define-key ruby-mode-map (kbd "C-c l") "lambda")))
 
