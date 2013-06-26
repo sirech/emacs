52c347487af9f9e6939e68fa124afb8eeb6053ec
Mario Fernandez <mario.fernandez@amiando.com>
2013-06-26 14:28:35 +0200

    Remove bash from flymake

--- lang/lang-bash.el
+++ lang/lang-bash.el
@@ -3,22 +3,6 @@
 ;; Set hooks
 
 (add-hook 'sh-mode-hook 'run-coding-hook)
-(add-hook 'sh-mode-hook 'bash-activate-ide)
-
-;; Activation
-
-(defun bash-activate-ide ()
-  (turn-on-flymake nil))
-
-;; Initialization
-(defvar flymake-bash-err-line-patterns
-      '(("^\\(.+\\): line \\([0-9]+\\): \\(.+\\)$" 1 2 nil 3)))
-
-(eval-after-load 'flymake
-  '(setup-flymake nil "sh" 'flymake-bash-init nil flymake-bash-err-line-patterns))
-
-(defun flymake-bash-init ()
-  (custom-flymake-init "bash" "-n"))
 
 (provide 'lang-bash)
 ;;; lang-bash.el ends herea
