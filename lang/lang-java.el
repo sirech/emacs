8f975497a76598e044f025fad43988772f6dac15
Mario Fernandez <mario.fernandez@amiando.com>
2013-06-26 14:22:24 +0200

    Remove flymake for java

--- lang/lang-java.el
+++ lang/lang-java.el
@@ -5,13 +5,6 @@
 ;;
 ;;  * cedet: Install the package in the vendor dir.
 ;;
-;;  * eclipse batch compiler (ecj.jar):
-;;   - http://download.eclipse.org/eclipse/downloads/, choose release, go to JDT Core batch compiler
-;;   - put the jar file in the lib directory, renaming it to ecj.jar (remove version number)
-;;
-;; The functionality is turned on only if the required
-;; dependencies can be found.
-
 ;; Alternative guide:
 ;;  * eclipse:
 ;;   - http://eclipse.org/downloads/index.php
@@ -35,10 +28,6 @@
 (add-hook 'jde-mode-hook 'jde-activate-ide)
 
 ;; Check availability
-
-(defvar ecj-location (concat lib-dir "ecj.jar")
-  "The location of the ecj compiler")
-
 (defun eclim-is-present ()
   "Determines if the IDE can be run"
   (locate-library "eclim"))
@@ -49,12 +38,6 @@
    (locate-library "cedet")
    (locate-library "jde")))
 
-(defun ecj-is-present ()
-  "Determines if the syntax checker is present in the system"
-  (and
-   (jde-ide-is-present)
-   (file-exists-p ecj-location)))
-
 ;; Attach mode
 
 ;; Don't do this if jde-mode is not there
@@ -62,12 +45,6 @@
   (add-to-list 'auto-mode-alist '("\\.java$" . jde-mode)))
 
 ;; Activation
-
-(defun jde-activate-ide ()
-  (turn-on-flymake 'ecj-is-present))  
-
-;; (defun jde-activate-autocomplete ()
-;;   (add-to-list 'ac-sources 'ac-source-jde))
 
 (defun java-activate-ide ()
   (when (eclim-is-present)
@@ -82,12 +59,6 @@
 (when (jde-ide-is-present)
   (require 'cedet)
   (autoload 'jde-mode "jde" "JDE mode." t))
-
-;; TODO: this triggers the loading of jde and flymake
-(when (ecj-is-present)
-  (require 'jde-eclipse-compiler-server)
-  (setq jde-compiler (list (list "eclipse java compiler server" ecj-location)))
-  (setq jde-ecj-command-line-args (list "-d" "none" "-target" "1.6" "-source" "1.6" "-proceedOnError")))
 
 (defun load-eclim ()
   (require 'eclim)
@@ -120,83 +91,6 @@
 
      ;; VS-style compile controls
      (define-key jde-mode-map (kbd "<f7>") 'jde-compile)))
-
-;; Change flymake to use the ecj
-(eval-after-load 'flymake
-  '(setup-flymake 'ecj-is-present "java" 'jde-ecj-flymake-init 'jde-ecj-flymake-cleanup nil))
-
-;; Auto-completion
-
-;; (defvar ac-source-jde
-;;   '((candidates
-;;      . (lambda ()
-;;          (mapcar 'cdr (jde-autocomplete)))))
-;;   "Source for JDE")
-
-;; (defun jde-autocomplete ()
-;;   (interactive)
-;;   (let* ((pair (jde-parse-java-variable-at-point))
-;;          jde-parse-attempted-to-import)
-;;     ;;resetting jde-complete-current-list
-;;     (setq jde-complete-current-list nil)
-;;     (if pair
-;;         (condition-case err
-;;             (jde-autocomplete-pair (jde-complete-get-pair pair nil))
-;;           (error (condition-case err
-;;                      (jde-autocomplete-pair (jde-complete-get-pair pair t)))
-;;                  (error (message "%s" (error-message-string err)))))
-;;       (message "No completion at this point"))))
-
-;; (defun jde-autocomplete-pair (pair)
-;;   (let ((access (jde-complete-get-access pair))
-;;         completion-list)
-;;     (progn
-;;       (if access
-;;           (setq completion-list
-;;                 (jde-complete-find-completion-for-pair pair nil access))
-;;         (setq completion-list (jde-complete-find-completion-for-pair pair)))
-;;       ;;if the completion list is nil check if the method is in the current
-;;       ;;class(this)
-;;       (if (null completion-list)
-;;           (setq completion-list (jde-complete-find-completion-for-pair
-;;                                  (list (concat "this." (car pair)) "")
-;;                                  nil jde-complete-private)))
-;;       ;;if completions is still null check if the method is in the
-;;       ;;super class
-;;       (if (null completion-list)
-;;           (setq completion-list (jde-complete-find-completion-for-pair
-;;                                  (list (concat "super." (car pair)) "")
-;;                                  nil jde-complete-protected)))
-
-;;       (if completion-list
-;;           jde-complete-current-list
-;;         (error "No completion at this point")))))
-
-;; (defun my-jde-mode-hook ()
-;;   "Hook for running Java file..."
-;;   (message "Loading my-java-hook...")
-;;   (define-key c-mode-base-map "\C-ca" 'jde-javadoc-generate-javadoc-template)
-;;   (define-key c-mode-base-map "\C-m" 'newline-and-indent)
-;;   (c-set-offset 'substatement-open 0)
-;;   (c-set-offset 'statement-case-open 0)
-;;   (c-set-offset 'case-label '+)
-;;   (fset 'my-javadoc-code
-;;      [?< ?c ?o ?d ?e ?>?< ?/ ?c ?o ?d ?e ?> left left left left left left left])
-;;   (define-key c-mode-base-map "\C-cx" 'my-javadoc-code)
-;;   (abbrev-mode t)
-;;   (setq c-comment-continuation-stars "* "
-;;      tab-width 4
-;;      indent-tabs-mode nil
-;;      tempo-interactive t
-;;      c-basic-offset 4)
-;;   (message "my-jde-mode-hook function executed")
-;; )
-
-;; (add-hook 'jde-mode-hook 'my-jde-mode-hook)
-;; (add-hook 'jde-mode-hook 'load-cedet)
-
-;; (eval-after-load 'jde-mode
-;;   (load "vendor/cedet/common/cedet.el"))
 
 (provide 'lang-java)
 ;;; lang-java.el ends here
